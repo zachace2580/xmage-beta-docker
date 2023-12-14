fe2a7b663509193b21671b02e57b13a48a1de8a5
Jared Stys <jaredstys@gmail.com>
2023-12-14 12:18:30 -0500

    Changed to what I need

--- docker/alpine_openjdk8_from_beta/dockerStartServer.sh
+++ docker/alpine_openjdk8_from_beta/dockerStartServer.sh
@@ -9,4 +9,4 @@ sed -i -e "s#\(secondaryBindPort=\)[\"].*[\"]#\1\"$XMAGE_DOCKER_SEONDARY_BIND_PO
 sed -i -e "s#\(maxSecondsIdle=\)[\"].*[\"]#\1\"$XMAGE_DOCKER_MAX_SECONDS_IDLE\"#g" ${XMAGE_CONFIG}
 sed -i -e "s#\(authenticationActivated=\)[\"].*[\"]#\1\"$XMAGE_DOCKER_AUTHENTICATION_ACTIVATED\"#g" ${XMAGE_CONFIG}
 
-java -Xms256M -Xmx512M -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-*.jar
+java -Xms1G -Xmx2G -Djava.security.policy=./config/security.policy -XX:ActiveProcessorCount=4 -Djava.util.logging.config.file=./config/logging.config  -Dsun.java2d.opengl=true -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-*.jar
